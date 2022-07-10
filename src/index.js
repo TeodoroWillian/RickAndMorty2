c089e3a4fcf23f9de32aa559f65e34c97abd1516
TeodoroWillian <totawillian@gmail.com>
2022-07-10 19:04:27 -0400

    connect database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require('cors');
+const connectDatabase = require("../src/database/database");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
+connectDatabase();
+
 app.use(cors());
-app.get('/', (req,res) => {
-    res.send({message:"Hello World"});
-})
 
 app.listen(port, () =>{
     console.log(`Servidor rodando na porta ${port}`)
