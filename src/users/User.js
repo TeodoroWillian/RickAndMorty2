b880577807189c08211bbc05a693d85d75ec3fa2
TeodoroWillian <totawillian@gmail.com>
2022-07-10 21:05:54 -0400

    bcrypt

--- src/users/User.js
+++ src/users/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -25,6 +26,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required:true,
     }
+});
+
+userSchema.pre('save', async function (next) {
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
 });
 
 const User = mongoose.model("User", userSchema);
