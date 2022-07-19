9e9e225e8620a290cf2d3e46aecbe767bad10d6b
TeodoroWillian <totawillian@gmail.com>
2022-07-18 20:04:35 -0400

    creating characters

---
+++ src/characters/RickAndMorty.route.js
@@ -0,0 +1,31 @@
+const route = require("express").Router();
+const controllerCharacters = require("../controllers/rickAndMorty.controller");
+const {
+  validId,
+  validObjectBody,
+} = require("../middlewares/rickAndMorty.middleware");
+
+route.get("/all-characters", controllerCharacters.findAllCharactersController);
+route.get(
+  "/one-character/:id",
+  validId,
+  controllerCharacters.findByIdCharacterController
+);
+route.post(
+  "/create-character",
+  validObjectBody,
+  controllerCharacters.createCharacterController
+);
+route.put(
+  "/update-character/:id",
+  validId,
+  validObjectBody,
+  controllerCharacters.updateCharacterController
+);
+route.delete(
+  "/delete-character/:id",
+  validId,
+  controllerCharacters.deleteCharacterController
+);
+
+module.exports = route;
